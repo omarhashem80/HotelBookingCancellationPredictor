e65cfce27377a74521943068f6603c6a2d19c417
omarhashem <omarsayed4736@gmail.com>
2026-05-02 07:14:10 +0300

    fix

--- src/data/preprocess.py
+++ src/data/preprocess.py
@@ -53,10 +53,10 @@ def build_preprocessor(X: pd.DataFrame) -> ColumnTransformer:
 
     month_pipeline = Pipeline(
         steps=[
-            (
-                "month",
-                FunctionTransformer(lambda s: s.apply(lambda col: col.dt.month), validate=False),
-            ),
+            # (
+            #     "month",
+            #     FunctionTransformer(lambda s: s.apply(lambda col: col.dt.month), validate=False),
+            # ),
             ("imputer", SimpleImputer(strategy="most_frequent")),
         ]
     )
